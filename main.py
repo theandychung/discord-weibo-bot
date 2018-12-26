a83fd2adcd64127bce55ee9db630070a12c88b11
theandychung <theandychung@gmail.com>
2018-12-25 23:22:57 -0800

    fix exceptions

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from proxy import IPPool
 # init
 sleep_time = 60
 last_weibo_id = {}
-first_run_send = True  # false
+first_run_send = False  # false
 
 
 def worth(user_id, post_id):
@@ -40,28 +40,30 @@ def convert_content(html_content):
 hook = Webhook(data_json["Discord"]["webhook_url"])
 proxy = IPPool().get_sslproxies_ip
 while True:
-    # try:
+    try:
         # print("fetching without username/passwords")
         # loop B: fetch without username/passwords
-    client = WeiboClient(proxies=proxy)
-    h = html2text.HTML2Text()
-    h.ignore_links = True
-    for za_id in data_json["Weibo"]["weibo_id"]:
-        p = client.people(za_id)
-        for status in p.statuses.page(1):
-            if status.isTop is None:
-                if worth(za_id, status.id):
-                    embed = None
-                    if status.original_pic is not None:
-                        embed = Embed(image_url=status.original_pic)
-                    hook.send(convert_content(status.longTextContent),
-                              username=p.name,
-                              avatar_url=p.avatar,
-                              embed=embed)
-                    # print("sent")
-                    break
-    # except Exception as e:
-    #     print(e)
-    #     print("get new ip")
-    #     proxy = IPPool().get_sslproxies_ip
+        client = WeiboClient(proxies=proxy)
+        h = html2text.HTML2Text()
+        h.ignore_links = True
+        for za_id in data_json["Weibo"]["weibo_id"]:
+            p = client.people(za_id)
+            for status in p.statuses.page(1):
+                if status.isTop is None:
+                    if worth(za_id, status.id):
+                        embed = None
+                        if status.original_pic is not None:
+                            embed = Embed(image_url=status.original_pic)
+                        hook.send(convert_content(status.longTextContent),
+                                  username=p.name,
+                                  avatar_url=p.avatar,
+                                  embed=embed)
+                        # print("sent")
+                        break
+    except ConnectionError as e:
+        print(e)
+        print("get new ip")
+        proxy = IPPool().get_sslproxies_ip
 
+    except Exception:
+        break
